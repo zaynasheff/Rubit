2e2af5b904a07634b24b091a9e4254eaed8b21ae
zaynasheff <zaynasheff@gmail.com>
2018-02-25 10:23:10 +0300

    home page

--- rubit/resources/views/auth/passwords/reset.blade.php
+++ rubit/resources/views/auth/passwords/reset.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3>
-                        Сброс пароля
+                        Смена пароля
                     </h3>
                 </div>
                 <div class="panel-body">
--- rubit/resources/views/home.blade.php
+++ rubit/resources/views/home.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Тут будет кабинет</div>
 
-                <div class="panel-body">
+    <div id="cab">
+
+
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+        <h4>Вы авторизованы</h4>
+
+
+
+        <h1>Тут будет кабинет юзера</h1>
+
     </div>
-</div>
+
+
 @endsection
